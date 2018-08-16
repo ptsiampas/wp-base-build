f841ff582f9f67e8df0a28b626142c37fd702186
Peter Tsiampas <peter@tsiampas.com>
2018-08-16 16:51:25 +1000

    Fixed misconfiguration for what network was being used by docker-compose and fixed secret name for mysql.

--- tools/gen-secrets.sh
+++ tools/gen-secrets.sh
@@ -11,7 +11,7 @@ MYSQL_ROOT_PASS=""
 WP_DB_USER=""
 WP_DB_PASS=""
 
-echo ${MYSQL_ROOT_PASS} | docker secret create ${CONTAINER_NAME}-msql-root -;
+echo ${MYSQL_ROOT_PASS} | docker secret create ${CONTAINER_NAME}-mysql-root-pass -;
 echo ${WP_DB_USER} | docker secret create ${CONTAINER_NAME}-wp-user -;
 echo ${WP_DB_PASS} | docker secret create ${CONTAINER_NAME}-wp-user-pass -;
 
