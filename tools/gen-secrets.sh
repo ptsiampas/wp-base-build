b6fc37d044a989e796558824610d86c6b7ece5f3
Peter Tsiampas <peter@tsiampas.com>
2018-08-16 15:50:45 +1000

    Updated Genearate Secrects config script

--- tools/gen-secrets.sh
+++ tools/gen-secrets.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-for x in laserkraft-msql-root laserkraft-wp-user laserkraft-wp-user-pass; 
-do 
- openssl rand -base64 20 | docker secret create ${x} -; 
-done
+
+if [ ! -f ../env ]; then
+    echo "Unable to file env file."
+    exit
+fi
+
+source ../env
+
+MYSQL_ROOT_PASS=""
+WP_DB_USER=""
+WP_DB_PASS=""
+
+echo ${MYSQL_ROOT_PASS} | docker secret create ${CONTAINER_NAME}-msql-root -;
+echo ${WP_DB_USER} | docker secret create ${CONTAINER_NAME}-wp-user -;
+echo ${WP_DB_PASS} | docker secret create ${CONTAINER_NAME}-wp-user-pass -;
+
 
