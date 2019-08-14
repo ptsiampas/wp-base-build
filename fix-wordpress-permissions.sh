090bd5c3fa828c86a52b5d6b1d2649856a3582d8
Peter Tsiampas <ptsiampas@users.noreply.github.com>
2019-08-14 13:25:34 +1000

    Updated find to only change what is different

--- fix-wordpress-permissions.sh
+++ fix-wordpress-permissions.sh
@@ -4,22 +4,22 @@
 # from http://codex.wordpress.org/Hardening_WordPress#File_permissions
 #
 # Author: Michael Conigliaro <mike [at] conigliaro [dot] org>
-#
+# Modfified to be more efficent by Peter Tsiampas <peter [at] wiredelf [dot] com>
 WP_OWNER=33 # <-- wordpress owner
 WP_GROUP=33 # <-- wordpress group
 WP_ROOT=$(pwd)/data/wp # <-- wordpress root directory
 WS_GROUP=33 # <-- webserver group
 
 # reset to safe defaults
-find ${WP_ROOT} -exec chown ${WP_OWNER}:${WP_GROUP} {} \;
-find ${WP_ROOT} -type d -exec sudo chmod 755 {} \;
-find ${WP_ROOT} -type f -exec sudo chmod 644 {} \;
+find ${WP_ROOT} ! -user ${WP_OWNER} ! -group ${WP_GROUP}  -exec chown ${WP_OWNER}:${WP_GROUP} {} \;
+find ${WP_ROOT} -type d -not -perm 755 -exec sudo chmod 755 {} \;
+find ${WP_ROOT} -type f -not -perm 644 -exec sudo chmod 644 {} \;
 
 # allow wordpress to manage wp-config.php (but prevent world access)
 sudo chgrp ${WS_GROUP} ${WP_ROOT}/wp-config.php
 sudo chmod 660 ${WP_ROOT}/wp-config.php
 
 # allow wordpress to manage wp-content
-find ${WP_ROOT}/wp-content -exec sudo chgrp ${WS_GROUP} {} \;
-find ${WP_ROOT}/wp-content -type d -exec sudo chmod 775 {} \;
-find ${WP_ROOT}/wp-content -type f -exec sudo chmod 664 {} \;
+find ${WP_ROOT}/wp-content ! -group ${WS_GROUP}  -exec sudo chgrp ${WS_GROUP} {} \;
+find ${WP_ROOT}/wp-content -type d -not -perm 775  -exec sudo chmod 775 {} \;
+find ${WP_ROOT}/wp-content -type f -not -perm 665 -exec sudo chmod 664 {} \;
